4c76eaed0a199bd5cedc5f39a5f6dc95802a3f5c
Erik Plachta <eplachta@gmail.com>
2022-04-03 19:25:21 -0400

    Convert auth.js to non-restful. Archive RESTful routes

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
-//TODO:: 04/03/2022 #EP || Add as env variable
 const secret = 'mysecretsshhhhh';
 // const secret = proces.env.JWT_SECRET;
 const expiration = '2h';
@@ -9,29 +8,29 @@ const expiration = '2h';
 module.exports = {
   // function used by authenticated routes
   authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token
+        .split(' ')
+        .pop()
+        .trim();
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
-    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
-
-    // send to next endpoint
-    next();
+   
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
@@ -40,36 +39,3 @@ module.exports = {
   },
 };
 
-//TODO:: 04/03/2022 #EP || Update this with basic GraphQL framework as below
-// module.exports = {
-//   authMiddleware: function({ req }) {
-//     // allows token to be sent via req.body, req.query, or headers
-//     let token = req.body.token || req.query.token || req.headers.authorization;
-
-//     // ["Bearer", "<tokenvalue>"]
-//     if (req.headers.authorization) {
-//       token = token
-//         .split(' ')
-//         .pop()
-//         .trim();
-//     }
-
-//     if (!token) {
-//       return req;
-//     }
-
-//     try {
-//       const { data } = jwt.verify(token, secret, { maxAge: expiration });
-//       req.user = data;
-//     } catch {
-//       console.log('Invalid token');
-//     }
-
-//     return req;
-//   },
-//   signToken: function({ username, email, _id }) {
-//     const payload = { username, email, _id };
-
-//     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-//   }
-// };
